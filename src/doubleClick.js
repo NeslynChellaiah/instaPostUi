5deac19e40d6c14326acf84c10539f56ec077cbd
Neslyn <neslynchellaiah@gmail.com>
2021-11-07 12:11:28 +0530

    hotfix

--- src/doubleClick.js
+++ src/doubleClick.js
@@ -2,18 +2,23 @@ import animateHeartIcon from './animateHeartIcon';
 
 export default () => {
     let count = 0
+    let flag = true
     return (id, heartId, fillHeartId) => {
         count += 1
         let heart = document.getElementById(id);
-        if(count == 2){
+        if(count == 2 && flag){
+            flag = false
             heart.style.display = "inline-block"
             heart.classList.add("heart-active")
             animateHeartIcon("emptyHeart", heartId, fillHeartId)
             setTimeout(()=>{
-                count = 0
+                flag = true
                 heart.classList.remove("heart-active")
                 heart.style.display = "none"
             }, 1000)
         }
+        setTimeout(()=>{
+            count = 0
+        }, 300)
     }
 }
